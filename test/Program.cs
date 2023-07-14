7592c6de06a90294a1229f5ffb9e0ad5d8e2aa96
captain-constantine-margarynt <89223974+captain-constantine-margarynt@users.noreply.github.com>
2023-07-15 00:53:34 +0300

    Test work_code

---
+++ test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+Clear();
+WriteLine("Введите через пробел элементы массива и нажмите ENTER: ");
+string phrase = ReadLine();
+
+string[] words = phrase.Split(' ');
+
+WriteLine();
+WriteLine("Вы ввели: ");
+Console.WriteLine(String.Join(", ", words));
+
+int counter = 0; 
+foreach (string s in words) {
+    if (s.Length <= 3){
+    counter++;
+    } 
+}
+
+WriteLine(counter);
+
+string [] result = new string [counter];
+int i =0;
+foreach(string s in words){
+    if (s.Length <= 3){
+        result[i] = s;
+        i++;
+}
+}
+
+WriteLine();
+WriteLine("Новый массив: ");
+Console.WriteLine(String.Join(", ", result));
